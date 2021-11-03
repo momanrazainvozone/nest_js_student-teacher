2fb967d6270561f70996e1f09ba57f8fd27f2858
utf4 <furqan_aziz@invozone.com>
2021-11-03 18:23:16 +0500

    Done with crone job

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { StudentsModule } from '../student/student.module';
 import { TeacherModule } from '../teacher/teacher.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
+import { Cron } from '@nestjs/schedule';
 import { config } from '../config/orm.config';
 require('dotenv').config();
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), StudentsModule, TeacherModule],
+  imports: [
+    TypeOrmModule.forRoot(config),
+    ScheduleModule.forRoot(),
+    StudentsModule,
+    TeacherModule,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  @Cron('* * * * * *')
+  log() {
+    console.log('Every seconds');
+  }
+}
